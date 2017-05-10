e048d2e77295fe9e66c7c0bf427ed77c3b576bec
luislocon <luis.locon@gmail.com>
2017-05-10 02:00:01 -0600

    finish project

--- js/main.js
+++ js/main.js
@@ -1,34 +1,58 @@
 $(document).ready(function(){
-
+  var beep = $('#beep')[0];
     var countTime = parseInt($('#num').html());
     var countBreak = parseInt($('#breakNum').html());
     $('#reset').hide();
     //Start event
     $('#start').click(function(){
       var counter = setInterval(timer, 1000);
+      countTime=countTime*60;
+      countBreak=countBreak*60;
       //Timer function
       function timer(){
         //Hide variables
-        $('#start, #minusTime, #plusTime', '#plusBreak', '#minusBreak').hide();
+        $('#start').hide();
         countTime-=1;
         $('#titleTime').html('SESSION TIME')
-        $('#timerType').html(countTime);
+        //$('#timerType').html(countTime);
         if(countTime===0){
           clearInterval(counter);
+          beep.play();
           var startBreak = setInterval(breakTimer, 1000);
+        }
+        if(countTime%60>=10){
+          $('#timerType').html(Math.floor(countTime/60)+':'+countTime%60);
+        }else{
+          $('#timerType').html(Math.floor(countTime/60)+':0'+countTime%60);
         }
         //BreakTime function
         function breakTimer(){
           countBreak-=1;
           $('#titleTime').html('BREAK TIME')
-          $('#timerType').html(countBreak);
           if(countBreak===0){
             clearInterval(startBreak);
+            beep.play();
             $('#reset').show();
+          }
+          if(countBreak%60>=10){
+            $('#timerType').html(Math.floor(countBreak/60)+':'+countBreak%60);
+          }else{
+            $('#timerType').html(Math.floor(countBreak/60)+':0'+countBreak%60);
           }
         }
       }
 
+    });
+
+    //Reset event
+    $('#reset').click(function(){
+      countTime = 25;
+      countBreak = 5;
+      $('#num').html(countTime);
+      $('#breakNum').html(countBreak);
+      $('#start').show();
+      $('#titleTime').html('TIME')
+      $('#timerType').html("");
     });
 
     $('#minusTime').click(function(){
